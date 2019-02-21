53bb67ef96cd489eb028a28249a0b4639f1ed007
jesseilev <jesseilev@gmail.com>
2019-02-21 15:25:11 -0500

    Updates

---
+++ precache-manifest.d7cd4ae33b67a0dec6fc4c5938850729.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d00353a499b95137a22d",
+    "url": "/parambre/static/js/runtime~main.d00353a4.js"
+  },
+  {
+    "revision": "2c7c3b6732d37458cecb",
+    "url": "/parambre/static/js/main.2c7c3b67.chunk.js"
+  },
+  {
+    "revision": "9fc150372ef3beb11da6",
+    "url": "/parambre/static/js/1.9fc15037.chunk.js"
+  },
+  {
+    "revision": "f26185b1ac319838386b579969aa82c0",
+    "url": "/parambre/index.html"
+  }
+];
