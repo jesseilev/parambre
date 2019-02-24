2a3b42d031b671ee560a7d4a2d6edfd6c1def057
jesseilev <jesseilev@gmail.com>
2019-02-24 12:38:05 -0500

    unlock audiocontext on ios devices

--- src/audioContext.js
+++ src/audioContext.js
@@ -1,20 +1,35 @@
-const tryAudioContext = () => {
-  if (window == 'undefined') {
+
+export default (function() {
+  const tryAudioContext = () => {
+    if (window == 'undefined') {
+      return false;
+    }
+    return window.AudioContext // Default
+      || window.webkitAudioContext // Safari and old versions of Chrome
+      || false; 
+  }
+
+  const AudioContext = tryAudioContext();
+
+  if (! AudioContext) {
+    // crap out early
+    alert("Sorry, but the Web Audio API is not supported by your browser." 
+      + "Please, consider upgrading to the latest version or downloading "
+      + "Google Chrome or Mozilla Firefox."
+    );
     return false;
   }
-  return window.AudioContext // Default
-    || window.webkitAudioContext // Safari and old versions of Chrome
-    || false; 
-}
 
-const AudioContext = tryAudioContext();
+  const context = new AudioContext();
 
-if (! AudioContext) {
-  alert("Sorry, but the Web Audio API is not supported by your browser." 
-    + "Please, consider upgrading to the latest version or downloading "
-    + "Google Chrome or Mozilla Firefox."
-  );
-}
+  // unlock if iOS
+  const isLockedOnIosDevice = context.state === 'suspended' && 'ontouchstart' in window;
+  if(isLockedOnIosDevice) {
+    const unlock = () => {
+      context.resume();
+    }
+    document.body.addEventListener('touchstart', unlock, false);
+  }
 
-
-export default new AudioContext();
+  return context;
+})();
